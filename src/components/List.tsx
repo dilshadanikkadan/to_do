30c738becacc8960c51803d2fcb4fef146fece8b
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-04-21 09:50:39 +0530

    refactored the code

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,28 +1,32 @@
-import { ListType, ReactState } from "../types/type"
-import Button from "./Button"
+import { ListType, ReactState } from "../types/type";
+import Button from "./Button";
 
 type ListArray = {
-    todoList: ListType[];
-    setTodoList: ReactState<ListType[]>
-}
+  todoList: ListType[];
+  setTodoList: ReactState<ListType[]>;
+};
 
 const List = ({ todoList, setTodoList }: ListArray) => {
-    const handleDelete = (id: Number) => {
-        const updatedList = todoList.filter((x) => x.id !== id)
-        setTodoList(updatedList)
-    }
-    return (
-        <>
-            {
-                todoList.map((item, i) => (
-                    <div key={i} className="mt-3 ml-3 flex gap-2">
-                        <p className="w-[80%]  py-1 border-[1px] border-gray-600  text-center">{item?.value}</p>
-                        <Button onClick={() => handleDelete(item.id)} value="Del" className="py-1 px-3 bg-red-600 text-white" />
-                    </div>
-                ))
-            }
-        </>
-    )
-}
+  const handleDelete = (id: Number) => {
+    const updatedList = todoList.filter((x) => x.id !== id);
+    setTodoList(updatedList);
+  };
+  return (
+    <>
+      {todoList.map((item, i) => (
+        <div key={i} className="mt-3 ml-3 flex gap-2">
+          <p className="w-[80%]  py-1 border-[1px] border-gray-600  text-center">
+            {item?.value}
+          </p>
+          <Button
+            onClick={() => handleDelete(item.id)}
+            value="Del"
+            className="py-1 px-3 bg-red-600 text-white"
+          />
+        </div>
+      ))}
+    </>
+  );
+};
 
-export default List
+export default List;
